ea84c90599e23148815b8a777a8d779684c4e8d1
perocrypto <81944259+perocrypto@users.noreply.github.com>
2024-07-26 23:22:58 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,32 @@
 import socket
-import threading
 
-def handle_client(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        client_socket.sendall(data)
+# Создаем сокет сервера
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Получаем локальный IP-адрес и порт
+host = '194.87.98.51'
+port = 8000
+
+# Связываем сокет с IP-адресом и портом
+server_socket.bind((host, port))
+
+# Начинаем прослушивание входящих подключений
+server_socket.listen(5)
+
+print(f"Сервер запущен. Слушаем {host}:{port}")
+
+while True:
+    # Ждем подключения клиента
+    client_socket, address = server_socket.accept()
+    print(f"Новое подключение от {address}")
+
+    # Получаем сообщение от клиента
+    message = client_socket.recv(1024).decode()
+    print(f"Получено сообщение: {message}")
+
+    # Отправляем ответ клиенту
+    response = f"Сервер получил сообщение: {message}"
+    client_socket.sendall(response.encode())
+
+    # Закрываем соединение с клиентом
     client_socket.close()
-
-def start_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print(f'Server started. Listening for incoming connections on {host}:{port}...')
-
-    while True:
-        client_socket, address = server_socket.accept()
-        print(f'Connected by {address}')
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-
-if __name__ == '__main__':
-    host = '194.87.98.51'  # Замените на свой IP-адрес
-    port = 8250
-    start_server(host, port)
